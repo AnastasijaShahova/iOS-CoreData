81c37c9e3c8a3981df0f1ed07e4da035423b8e34
Anastasia Shakhova <70802206+AnastasijaShahova@users.noreply.github.com>
2023-10-29 13:01:36 +0300

    Add files via upload

---
+++ MealTime/CoreData Entities/Meal+CoreDataProperties.swift
@@ -0,0 +1,18 @@
+import Foundation
+import CoreData
+
+
+extension Meal {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Meal> {
+        return NSFetchRequest<Meal>(entityName: "Meal")
+    }
+
+    @NSManaged public var date: Date?
+    @NSManaged public var user: User?
+
+}
+
+extension Meal : Identifiable {
+
+}
